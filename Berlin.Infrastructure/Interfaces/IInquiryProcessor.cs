07f2fcf3f40ec993a8026690ceedc21bdad469c2
Andrei M <marin_andrei@hotmail.com>
2020-04-14 00:20:57 +0300

    Intermediary, not tested

---
+++ Berlin.Infrastructure/Interfaces/IInquiryProcessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Berlin.Infrastructure.Interfaces
+{
+    public interface IInquiryProcessor
+    {
+        IEnumerable<IProductCost> Execute(long kilowatts, long months);
+    }
+}
---
+++ Berlin.Infrastructure/Interfaces/IProductCost.cs
@@ -0,0 +1,8 @@
+﻿namespace Berlin.Infrastructure.Interfaces
+{
+    public interface IProductCost
+    {
+        string Name { get; set; }
+        decimal Amount { get; set; }
+    }
+}
